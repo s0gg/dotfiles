205688d1280f98da9fd61bda78a2baa6230f0858
s0gg <s0gg.dev@gmail.com>
2024-01-14 20:09:15 +0900

    fish: update

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_add_path /Users/kamesuke42/.nix-profile/bin
+    fish_add_path $HOME/.nix-profile/bin
     fish_add_path /nix/var/nix/profiles/default/bin
+    fish_add_path $HOME/.volta/bin
+    fish_add_path /usr/local/opt/ruby/bin
+    fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.deno/bin
 end
