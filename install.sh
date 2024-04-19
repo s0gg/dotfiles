cdd6b66b0f0e06608690d469482d340488cef5d3
s0gg <mail@s0gg.net>
2024-04-19 23:21:18 +0900

    Add emacs symlink

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ echo "INSTALL s0gg's dotfiles..."
 "Create symlink for .wezterm.lua"
 ln -s `pwd`/.wezterm.lua $HOME/.wezterm.lua
 
+"Create symlink for .emacs.d"
+ln -s `pwd`/.emacs.d $HOME/.emacs.d
+
 echo "Done."
