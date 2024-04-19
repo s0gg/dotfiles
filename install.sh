3939ebd9fbe4de7a07267808250b9d448789d960
s0gg <mail@s0gg.net>
2024-04-19 23:22:28 +0900

    Add nvim

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ ln -s `pwd`/.wezterm.lua $HOME/.wezterm.lua
 "Create symlink for .emacs.d"
 ln -s `pwd`/.emacs.d $HOME/.emacs.d
 
+"Create symlink for .emacs.d"
+ln -s `pwd`/nvim $HOME/.config/nvim
+
 echo "Done."
---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+-- s0gg's nvim config
