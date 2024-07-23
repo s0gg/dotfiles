28dc69c684a6d8a6c3e554d9f60f4bb222fa84af
s0gg <mail@s0gg.net>
2024-07-24 00:03:41 +0900

    emacs: update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,6 +38,8 @@
 (add-to-list 'package-archives '("gnu-elpa-devel" . "https://elpa.gnu.org/devel/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
+(set-face-attribute 'default nil :font "HackGen Console NF" :height 120)
+
 (setq package-archive-priorities
       '(("gnu-elpa-devel" . 3)
         ("melpa" . 2)
@@ -50,3 +52,21 @@
 (use-package use-package
   :config
   (setq use-package-always-ensure t))
+
+(use-package catppuccin-theme
+  :ensure t
+  :config
+  (load-theme 'catppuccin :no-confirm))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(catppuccin-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
