98ad9991cbbdd5be5f764f2da0f5ff10f6869361
s0gg <mail@s0gg.net>
2024-07-23 23:11:34 +0900

    nvim: astro

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,6 +35,15 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "astro" },
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end
+})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
