8e2e6606be543889b19398cf356c9f7d23b8ce94
s0gg <mail@s0gg.net>
2024-07-24 08:58:31 +0900

    Emacs: Remove doom-modeline

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,22 +82,6 @@
         fci-rule-color "darkgray")
   (add-hook 'prog-mode-hook 'fci-mode))
 
-(use-package doom-modeline
-  :ensure t
-  :custom
-  (doom-modeline-buffer-file-name-style 'truncate-with-project)
-  (doom-modeline-icon t)
-  (doom-modeline-major-mode-icon nil)
-  (doom-modeline-minor-modes nil)
-  :hook
-  (after-init . doom-modeline-mode)
-  :config
-  (line-number-mode 0)
-  (column-number-mode 0)
-  (doom-modeline-def-modeline 'main
-  '(bar workspace-number window-number evil-state god-state ryo-modal xah-fly-keys matches buffer-info remote-host buffer-position parrot selection-info)
-  '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker)))
-
 (use-package org-bullets
   :ensure t
   :config
