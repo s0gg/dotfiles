3e7cf59139e041985288b43e45e5b25f31f8a24a
s0gg <mail@s0gg.net>
2024-07-24 08:57:22 +0900

    emacs: update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,6 +42,11 @@
 
 (set-face-attribute 'default nil :font "HackGen Console NF" :height 120)
 
+;; org-mode
+(global-set-key "\C-ca" 'org-agenda)
+(setq org-directory "~/org/"
+      org-agenda-files '("~/org/idea.org" "~/org/tasks.org" "~/org/target.org"))
+
 (setq package-archive-priorities
       '(("gnu-elpa-devel" . 3)
         ("melpa" . 2)
@@ -111,7 +116,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(catppuccin-theme doom-modeline hydra org-bullets)))
+ '(package-selected-packages
+   '(catppuccin-theme doom-modeline hydra org-agenda org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
