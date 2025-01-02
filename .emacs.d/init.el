448c8b30bab557520599c0c279d2c045cc1dd1c1
s0gg <mail@s0gg.net>
2025-01-02 15:36:59 +0900

    feat(emacs): Load a custom file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -557,3 +557,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(if (file-exists-p "custom.el")
+    (load-file "custom.el"))
