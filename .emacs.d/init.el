753d74f7a948aeec36c22a5384ae814d5a2b3779
s0gg <mail@s0gg.net>
2025-01-05 20:03:57 +0900

    feat(emacs): Change font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,8 +17,11 @@
 (setq coding-system-for-read 'utf-8-unix)
 (setq coding-systme-for-write 'utf-8-unix)
 
-;; (add-to-list 'default-frame-alist '(font . "0xProto-14"))
-;; (set-fontset-font "fontset-default" 'unicode "HackGen Console NF")
+(add-to-list 'default-frame-alist '(font . "HackGen Console NF-10"))
+(set-face-attribute 'default t
+                    :family "HackGen Console NF"
+                    :height 100)
+(set-frame-font "HackGen Console NF-10" nil t)
 
 (setopt make-backup-files nil)
 (setopt backup-by-copying t)
