392341e503dbd270e903b458adeb2645b6cbb0d9
s0gg <mail@s0gg.net>
2025-01-05 18:11:35 +0900

    feat(nvim): Ruby on Ralis

---
+++ nvim/lua/plugins/ror.lua
@@ -0,0 +1,23 @@
+return {
+  'weizheheng/ror.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'rcarriga/nvim-notify',
+    'stevearc/dressing.nvim'
+  },
+  config = function()
+    require("dressing").setup({
+      input = {
+        min_width = { 60, 0.9 },
+      },
+      select = {
+        -- telescope = require('telescope.themes').get_ivy({...})
+        telescope = require('telescope.themes').get_dropdown({ layout_config = { height = 15, width = 90 } }),
+      }
+    })
+    require 'ror'.setup {}
+
+    vim.keymap.set("n", "<Leader>rc", ":lua require('ror.commands').list_commands()<CR>", { silent = true })
+  end
+}
