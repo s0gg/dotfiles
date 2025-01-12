d84ffd488df9441e73c4a4993234a9b44cbbc4af
s0gg <mail@s0gg.net>
2025-01-12 17:49:17 +0900

    feat(nvim): Add conjure

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -496,5 +496,26 @@ return {
   {
     'thinca/vim-qfreplace',
     event = 'VeryLazy'
-  }
+  },
+  {
+    "Olical/conjure",
+    ft = { "clojure", "fennel", "python" }, -- etc
+    lazy = true,
+    init = function()
+      vim.g["conjure#mapping#doc_word"] = "gk"
+    end,
+
+    -- Optional cmp-conjure integration
+    dependencies = { "PaterJason/cmp-conjure" },
+  },
+  {
+    "PaterJason/cmp-conjure",
+    lazy = true,
+    config = function()
+      local cmp = require("cmp")
+      local config = cmp.get_config()
+      table.insert(config.sources, { name = "conjure" })
+      return cmp.setup(config)
+    end,
+  },
 }
