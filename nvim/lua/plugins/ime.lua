7ce186582bfb9ad1e21092493d6b875d7334081c
s0gg <mail@s0gg.net>
2025-01-24 00:36:37 +0900

    feat(nvim): Add skkeleton settings

--- nvim/lua/plugins/ime.lua
+++ nvim/lua/plugins/ime.lua
@@ -3,6 +3,21 @@ return {
     'vim-skk/skkeleton',
     dependencies = { 'vim-denops/denops.vim' },
     event = 'VeryLazy',
+    config = function()
+      vim.keymap.set('i', '<C-j>', '<Plug>(skkeleton-enable)', { silent = true })
+      vim.keymap.set('c', '<C-j>', '<Plug>(skkeleton-enable)', { silent = true })
+      vim.keymap.set('t', '<C-j>', '<Plug>(skkeleton-enable)', { silent = true })
+
+      vim.cmd([[
+        call skkeleton#config({
+          \ 'eggLikeNewline': v:true,
+          \ 'globalDictionaries': ["~/.local/share/skk/SKK-JISYO.L"]
+          \ })
+      ]])
+      vim.cmd([[
+        call skkeleton#register_keymap('input', ';', 'henkanPoint')
+      ]])
+    end
   }
 }
 
