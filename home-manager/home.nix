2f5e08dd7e65c3d2bd33042484726979853b9567
s0gg <mail@s0gg.net>
2025-03-13 01:00:17 +0900

    Install iccheck with home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -34,6 +34,32 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+    (pkgs.buildGoModule {
+      pname = "iccheck";
+      version = "0.9.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "salab";
+        repo = "iccheck";
+        rev = "v0.9.0";
+        sha256 = "sha256-2bD5gN/7C79njrCVoR5H2ses6pWAQHZcYj7/f2+Ui/o=";
+      };
+      vendorHash = "sha256-pqjtoshoQlz+SFpaaxN3GMaDdZ+ztiIV6w+CTrRHuaA=";
+      meta = with lib; {
+        homepage = "https://github.com/salab/iccheck";
+      };
+      doCheck = false;
+      subPackages = [
+        "."
+        "./cmd"
+        "./pkg/domain"
+        "./pkg/fleccs"
+        "./pkg/lsp"
+        "./pkg/ncdsearch"
+        "./pkg/printer"
+        "./pkg/search"
+        "./pkg/utils"
+      ];
+    })
     pkgs.direnv
     pkgs.bat
     pkgs.eza
