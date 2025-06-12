49d1fa3aa8dd8b9f08ff046954f374cc46b80b4b
s0gg <mail@s0gg.net>
2025-06-12 23:22:02 +0900

    refactor(plugins): remove commented fall.vim config
    Removed the commented-out configuration block for fall.vim from the
    plugins/ff.lua file. This cleanup improves readability and maintains focus
    on the active Telescope plugin configuration.

--- nvim/lua/plugins/ff.lua
+++ nvim/lua/plugins/ff.lua
@@ -1,16 +1,4 @@
 return {
-  -- {
-  --   'vim-fall/fall.vim',
-  --   dependencies = { 'vim-denops/denops.vim' },
-  --   event = 'VeryLazy',
-  --   config = function()
-  --     vim.keymap.set('n', '<leader>ff', '<cmd>Fall file<cr>', { silent = true })
-  --     vim.keymap.set('n', '<leader>fg', '<cmd>Fall rg<cr>', { silent = true })
-  --     vim.keymap.set('n', '<leader>fb', '<cmd>Fall buffer<cr>', { silent = true })
-  --     vim.keymap.set('n', '<leader>fh', '<cmd>Fall help<cr>', { silent = true })
-  --     vim.keymap.set('n', '<leader>fl', '<cmd>Fall line<cr>', { silent = true })
-  --   end
-  -- }
   {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
