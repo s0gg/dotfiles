b61822aa72d9d9ea78494e7ac6dabcf61545f41e
s0gg <mail@s0gg.net>
2025-11-10 17:47:45 +0900

    nvim: Add keymap for opening terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -54,6 +54,8 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+vim.keymap.set("n", "<space>ts", "<cmd>vsplit +term<cr>", { silent = true, desc = "Open terminal in vertical split" })
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
