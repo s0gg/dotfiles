80d2cf4a4d6421c3e596f83154863572c76b1d2c
s0gg <mail@s0gg.net>
2026-09-16 12:19:10 +0900

    nvim: Add a keymap for telescope lsp document symbols

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -367,6 +367,7 @@ return {
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 			vim.keymap.set("n", "<leader>fl", builtin.current_buffer_fuzzy_find, { desc = "Telescope find line" })
+			vim.keymap.set("n", "<leader>tls", builtin.lsp_document_symbols, { desc = "Telescope lsp document symbols" })
 		end,
 	},
 	{
